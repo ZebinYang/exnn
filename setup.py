448b9fd881cecc6bf56c84773e3a525af93e77e0
[zebinyang] <yangzb2010@hku.hk>
2021-09-22 13:54:39 +0800

    update email address

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(name='exnn',
       description='The enhanced explainable neural network with sparse, orthogonal and smooth constraints',
       url='https://github.com/ZebinYang/exnn',
       author='Zebin Yang',
-      author_email='yangzb2010@hku.hk',
+      author_email='yangzb2010@connect.hku.hk',
       license='GPL',
       packages=['exnn'],
       install_requires=[
-          'matplotlib>=2.2.2','tensorflow>=2.0.0', 'numpy>=1.15.2'],
+          'matplotlib>=2.2.2', 'tensorflow>=2.0.0', 'numpy>=1.15.2'],
       zip_safe=False)
